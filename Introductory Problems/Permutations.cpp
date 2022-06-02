6073f75a4d9d40e0802a7a19ae4f92463bc3235d
sL0p3 <shlokjakhotia@gmail.com>
2022-06-02 14:48:22 +0530

    updated

--- Introductory Problems/Permutations.cpp
+++ Introductory Problems/Permutations.cpp
@@ -13,7 +13,7 @@ int main()
     cin>>n;
     string s="";
     if (n==1) cout<<1<<endl;
-    else if (n<5) cout<<"NO SOLUTION"<<endl;
+    else if (n<4) cout<<"NO SOLUTION"<<endl;
     else {
         for (int i = 1;i<=n;i++){
             if (i%2== 0 ) cout<<i<<" ";
