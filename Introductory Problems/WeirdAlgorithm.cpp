c471436d6eac094f4405ad7dd9f54b3eb4cca4b4
sL0p3 <shlokjakhotia@gmail.com>
2022-06-02 06:10:45 +0530

    updated

---
+++ Introductory Problems/WeirdAlgorithm.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    // Code here
+    
+
+    return;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
