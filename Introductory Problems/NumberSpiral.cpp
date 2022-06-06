6470ebf9dfaa0fa2adfc3f4d94e6132bbba8e9ba
sL0p3 <shlokjakhotia@gmail.com>
2022-06-06 17:26:56 +0530

    updated

---
+++ Introductory Problems/NumberSpiral.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<vector<int>> vvi;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef map<char, int> mci;
+#define FOR(a, b, c) for (int(a) = (b); (a) < (c); ++(a))
+#define FORN(a, b, c) for (int(a) = (b); (a) <= (c); ++(a))
+#define FORD(a, b, c) for (int(a) = (b); (a) >= (c); --(a))
+#define forEach(a, b) for (auto &(a) : (b))
+#define fr(i, n) FOR(i, 0, n)
+#define frn(i, n) FORN(i, 1, n)
+#define sqr(x) ((LL)(x) * (x))
+#define pb push_back
+#define pp pop_back
+#define fi first
+#define se second
+#define mp make_pair
+#define all(a) (a).begin(), (a).end()
+#define alla(arr, sz) arr, arr + sz
+const int MOD = 1e9 + 7;
+
+void solve()
+{
+    // Code here
+    ll x,y;
+    cin>>x>>y;
+    if (x>y){
+        if (!(x&2)) cout<< x*x -y + 1<<endl;
+        else cout<<(x-1)*(x-1) + y<<endl;
+    }else{
+        if (!(y&1)) cout<< (y-1)*(y-1) + x<<endl;
+        else cout<< y*y - x+1<<endl;
+    }
+    return;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
