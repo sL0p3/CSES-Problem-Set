011681a55469e9e9ba9ca174eb052f0b6a3bcd46
sL0p3 <shlokjakhotia@gmail.com>
2022-06-13 12:44:28 +0530

    updateds

---
+++ Searching and Sorting/FerrisWheel.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<bool> vb;
+#define sqr(x) ((LL)(x) * (x))
+#define endl "\n"
+#define pb push_back
+#define pp pop_back
+#define fi first
+#define se second
+#define mp make_pair
+#define all(a) (a).begin(), (a).end()
+#define alla(arr, sz) arr, arr + sz
+const int M = 1e9 + 7;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    // Code here
+
+    return 0;
+}
